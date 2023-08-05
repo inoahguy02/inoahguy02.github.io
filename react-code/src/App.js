edbcddfad8c9787d4e0adb9ad03b1add3362e689
Noah Garland <noahgarland02@gmail.com>
2023-08-04 20:02:50 -0500

    Added react router along with home and project page

--- react-code/src/App.js
+++ react-code/src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import Projects from './Projects';
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h className="Name">Noah Garland</h>
-        {/*<nav>
-          <ul>
-            <li></li>
-          </ul>
-        </nav>*/}
-      </header>
-      <body>
-        <p>hello</p>
-      </body>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Link to="/"><h1 className="Name">Noah Garland</h1></Link>
+          <Link to="/projects">Projects</Link>
+        </header>
+
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
---
+++ react-code/src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div>
+            <main>
+                <p>This is the home page</p>
+            </main>
+        </div>
+    );
+}
+
+export default Home;
