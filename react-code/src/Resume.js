fe09b7d1f8367a5e1eedda3f6e8b6cf43a035f47
Noah Garland <noahgarland02@gmail.com>
2023-08-07 13:07:42 -0500

    Added pic of resume and link to view pdf

--- react-code/src/Resume.js
+++ react-code/src/Resume.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Res from "./Resume.JPG"
 
 function Resume() {
     return (
         <main>
-            <h1>This is the Resume page (In Progress)</h1>
+            <h1>Resume can be downloaded <a href="https://drive.google.com/file/d/11RW6_dhTV8Q1-WZfjJcho0GvqN8_D2su/view?usp=sharing">Here</a></h1>
+            <img src={Res} alt="Resume" />
         </main>
     );
 }
