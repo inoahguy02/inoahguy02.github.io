f7a3dc4508c23b032ea7cd2e69c9df263cbc3a20
Noah Garland <noahgarland02@gmail.com>
2023-08-09 17:01:58 -0500

    Centered Resume page

--- react-code/src/Resume.js
+++ react-code/src/Resume.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Res from "./Resume.JPG"
+import "./Resume.css"
 
 function Resume() {
     return (
+        
         <main>
-            <h1>Resume can be downloaded <a href="https://drive.google.com/file/d/11RW6_dhTV8Q1-WZfjJcho0GvqN8_D2su/view?usp=sharing">Here</a></h1>
-            <img src={Res} alt="Resume" />
+            <h2>Resume can be viewed <a href="https://drive.google.com/file/d/11RW6_dhTV8Q1-WZfjJcho0GvqN8_D2su/view?usp=sharing">Here</a></h2><br />
+            <img src={Res} alt="Resume" /><br /><br />
         </main>
+        
     );
 }
 
