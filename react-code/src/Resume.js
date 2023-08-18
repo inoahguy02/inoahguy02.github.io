0900d713ee67ef62f09b0f4e94288b6d9c756b48
Noah Garland <noahgarland02@gmail.com>
2023-08-18 11:13:32 -0500

    Made Resume page mobile friendly

--- react-code/src/Resume.js
+++ react-code/src/Resume.js
@@ -6,7 +6,7 @@ function Resume() {
     return (
         <main>
             <h2>Resume can be viewed <a href="https://drive.google.com/file/d/11RW6_dhTV8Q1-WZfjJcho0GvqN8_D2su/view?usp=sharing">Here</a></h2><br />
-            <img src={Res} alt="Resume" /><br /><br />
+            <img src={Res} alt="Resume" className="ResumeJPG" /><br /><br />
         </main>
     );
 }
